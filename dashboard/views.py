7eaf387807a79f96410cad31ec4c31516dd2ef9b
ayushjd098 <jadhavayush1100@gmail.com>
2021-01-09 22:47:02 +0530

    added admin page

---
+++ dashboard/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def dashboard(request):
+    context = {'title' : 'Home'}
+    return render(request,'dashboard/dashboard.html')
+
+def create_student(request):
+    context = {'title' : "Create Student"}
+    return render(request, 'dashboard/create_student.html')
+
+def create_teacher(request):
+    context = {'title' : "Create Teacher"}
+    return render(request, 'dashboard/create_teacher.html')
