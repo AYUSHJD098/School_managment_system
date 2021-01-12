fb8ed15a9097071bf31925f83ef0107ccb62bf07
ayushjd098 <jadhavayush1100@gmail.com>
2021-01-12 23:24:53 +0530

    added create student form and url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import dashboard
+from . import views
 
 urlpatterns = [
-    path('', dashboard, name="dashboard")
+    path('', views.dashboard, name="dashboard"),
+    path('create-student', views.create_student, name="create_student__form"),
 ]
